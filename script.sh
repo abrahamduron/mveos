e65d0e8ddfe6d23d6345b25456afbc3b6df3cd6e
Abraham Durón <109776479+abrahamduron@users.noreply.github.com>
2022-08-04 13:33:05 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+zypper refresh
+sudo service docker start
 docker pull node:16
 docker pull mongo:3.1
 docker pull owasp/zap2docker-stable
@@ -8,5 +10,9 @@ docker pull owasp/modsecurity-crs
 docker pull mysql
 docker pull postgres
 docker pull php
+docker pull python
+docker pull mariadb
+
+echo "<°Listo. Ejecución completa!!!"
 
 exit
