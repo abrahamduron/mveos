5b368f6915edb5f80bb953f1121f24d119bb65b2
Abraham Durón <109776479+abrahamduron@users.noreply.github.com>
2022-08-11 16:22:10 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -41,5 +41,32 @@ suseInsertService sshd
 #--------------------------------------
 baseSetRunlevel 5
 
+# --> Configuración de <Artwork>
+#mkdir -p /usr/share/icons/tdbsuse/
+cp -f /mveos_recursos/os/artwork/wallpapers/* /usr/share/wallpapers/openSUSEdefault/contents/images/
+# cp -f /root/mveos_recursos/os/artwork/wallpapers/1350x1080.jpg /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/
+# cp -f /root/mveos_recursos/os/artwork/wallpapers/1440x1080.jpg /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/
+# cp -f /root/mveos_recursos/os/artwork/wallpapers/1600x1200.jpg /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/
+# cp -f /root/mveos_recursos/os/artwork/wallpapers/1920x1080.jpg /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/
+# cp -f /root/mveos_recursos/os/artwork/wallpapers/1920x1200.jpg /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/
+# cp -f /root/mveos_recursos/os/artwork/wallpapers/3840x2400.jpg /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/
+# cp -f /root/mveos_recursos/os/artwork/wallpapers/5120x3200.jpg /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/
 
+chmod 644 /usr/share/wallpapers/openSUSEdefault/contents/images/*
+# chmod 644 /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1350x1080.jpg
+# chmod 644 /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1440x1080.jpg
+# chmod 644 /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1600x1200.jpg
+# chmod 644 /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1920x1080.jpg
+# chmod 644 /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1920x1200.jpg
+# chmod 644 /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/3840x2400.jpg
+# chmod 644 /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/5120x3200.jpg
+
+chown root:root /usr/share/wallpapers/openSUSEdefault/contents/images/*
+# chown root:root /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1350x1080.jpg
+# chown root:root /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1440x1080.jpg
+# chown root:root /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1600x1200.jpg
+# chown root:root /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1920x1080.jpg
+# chown root:root /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/1920x1200.jpg
+# chown root:root /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/3840x2400.jpg
+# chown root:root /build/image-root/usr/share/wallpapers/openSUSEdefault/contents/images/5120x3200.jpg
 
